cb3c3ef9a05afc36a68ebc67710a008a4e3f4e77
valentin1042 <valentin1042@gmail.com>
2016-04-20 15:21:56 -0500

    added history class

---
+++ 6x6TTT/6x6TTT/historyClass.vb
@@ -0,0 +1,30 @@
+﻿Public Class historyClass
+    Private name As String 'player = New player()
+    Private wins As Integer = 0
+    Private loss As Integer = 0
+    Private tie As Integer = 0
+    Private opponent As String
+
+    Public Sub New()
+        name.name = ""
+        wins = 0
+        loss = 0
+        tie = 0
+        opponent = ""
+    End Sub
+
+    Public Sub getScore()
+        'Retrieve scores from gameboard
+        'name = 
+        'wins =
+        'loss =
+        'tie =
+    End Sub
+    Public Sub setScore()
+        'open file
+        'Add the score to variable
+        'if score = wins then wins +=1
+        'if score = loss then loss +=1
+        'if score = tie then tie +=1
+    End Sub
+End Class
