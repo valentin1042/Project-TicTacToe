1eeb4b123e241943711befb77858b2889c0a370f
valentin1042 <valentin1042@gmail.com>
2016-04-25 14:57:47 -0500

    update login form

--- 6x6TTT/6x6TTT/loginForm.vb
+++ 6x6TTT/6x6TTT/loginForm.vb
@@ -31,74 +31,32 @@
     'Return:
     '   Message indicating if valid username
     Private Sub enterButton_Click(sender As Object, e As EventArgs) Handles enterButton.Click
-        'If Not nameError Then
-        username = usernameTextBox.Text
-        If count = 3 Then
-            GameBoard1.Board.player1.Register(username)
-        ElseIf count = 2 Then
-            loginSucess = GameBoard1.Board.player1.Login(username)
-            If loginSucess Then
-                username1 = username
-                usernameTextBox.Clear()
-                count -= 1
-            End If
-        ElseIf count = 1 Then
-            If username = username1 Then
-                errorLabel.Text = "Enter different username."
-            Else
-                loginSucess = GameBoard1.Board.player2.Login(username)
+        If Not nameError Then
+            username = usernameTextBox.Text
+            If count = 3 Then
+                GameBoard1.Board.player1.Register(username)
+            ElseIf count = 2 Then
+                loginSucess = GameBoard1.Board.player1.Login(username)
                 If loginSucess Then
-                    MsgBox("Now choose your settings.")
-                    Me.Close()
-                    setupMenuForm.Show()
+                    username1 = username
+                    usernameTextBox.Clear()
+                    count -= 1
+                End If
+            ElseIf count = 1 Then
+                If username = username1 Then
+                    errorLabel.Text = "Enter different username."
+                Else
+                    loginSucess = GameBoard1.Board.player2.Login(username)
+                    If loginSucess Then
+                        MsgBox("Now choose your settings.")
+                        Me.Close()
+                        setupMenuForm.Show()
+                    End If
                 End If
             End If
+        Else
+            errorLabel.Text = "Invalid username."
         End If
-        'Else
-        'errorLabel.Text = "Invalid username."
-        'End If
-        'searchFile()
-
-        'If errorLabel.Visible Then
-        '    errorLabel.Text = "Enter username"
-        'Else
-        '    If count = 2 Then
-        '        If guest Then
-        '            'setupMenuForm.player1RadioButton.Text = "guest"
-        '            mainMenuForm.Board.player1.name = "guest"
-        '        Else
-        '            'Open text file 
-        '            'Search text file 
-        '            'If found then return true 
-        '            'player.name = username
-        '            'setupMenuForm.player1RadioButton.Text = username
-        '            mainMenuForm.Board.player1.name = username
-        '        End If
-        '        count -= 1
-        '        usernameTextBox.Text = ""
-        '        ''Me.Hide()
-        '    ElseIf count = 1 Then
-        '        ''Me.Show()
-        '        If guest Then
-        '            'mainMenuForm.Board.player2.name = "guest"
-        '            'setupMenuForm.player2RadioButton.Text = "guest"
-        '            mainMenuForm.Board.player2.name = "guest"
-        '        Else
-        '            ''Open text file 
-        '            ''Search text file 
-        '            ''If found then return true 
-        '            mainMenuForm.Board.player2.name = username
-        '            'setupMenuForm.player2RadioButton.Text = username
-        '        End If
-        '        Me.Close()
-        '        setupMenuForm.Show()
-        '    End If
-        'End If
-        'If Me.Text = "Register" Then
-        '    Using usernameFile As New System.IO.StreamWriter("username.txt")
-        '        usernameFile.WriteLine(username)
-        '    End Using
-        'End If
     End Sub
 
     'usernameTextBox
@@ -110,20 +68,19 @@
     'Returns: 
     '   Message indicating if username is invalid
     Private Sub usernameTextBox_TextChanged(sender As Object, e As EventArgs) Handles usernameTextBox.TextChanged
-        guestRadioButton.Checked = False
-        'Try
-        '    guestRadioButton.Checked = False
-        '    username = usernameTextBox.Text
-        '    If username = "Vader" Or username = "vader\n" Or username = "" Or username = "guest\n" Then
-        '        nameError = True
-        '    Else
-        '        errorLabel.Text = ""
-        '        nameError = False
-        '    End If
-        'Catch
-        '    errorLabel.Text = "Error"
-        '    nameError = True
-        'End Try
+
+        Try
+            username = usernameTextBox.Text.ToUpper()
+            If username.Contains("VADER") Or username.Length = 0 Or username.Contains(" ") Then
+                nameError = True
+            Else
+                errorLabel.Text = ""
+                nameError = False
+            End If
+        Catch
+            errorLabel.Text = "Error"
+            nameError = True
+        End Try
     End Sub
 
     'setCount
@@ -147,33 +104,9 @@
     Private Sub usernameTextBox_GotFocus(sender As Object, e As EventArgs) Handles usernameTextBox.GotFocus
         guestRadioButton.Checked = False
     End Sub
-    'Private Sub searchFile()
-    '    Try
-    '        Dim usernameFile = IO.File.ReadAllText("username.txt")
-    '        Dim lookfor As String = username
-    '        If usernameFile.Contains(lookfor) Then
-    '            errorLabel.Text = "Username found"
-    '            errorLabel.Visible = False
-
-    '        Else
-    '            errorLabel.Text = "Username not found"
-    '            errorLabel.Visible = True
-    '        End If
-    '        'Using usernameFile As New System.IO.StreamWriter("username.txt")
-    '        '    usernameFile.WriteLine(username)
-    '        'End Using
-    '    Catch
-    '        errorLabel.Text = "Error with file"
-
-    '    End Try
-
-    'End Sub
-
     'Things left to do 
     '1. Make function to look for username in file 
     '2. Error handling vader and guest as username
     '3. Testing 
-
-
 
 End Class
--- 6x6TTT/6x6TTT/playerClass.vb
+++ 6x6TTT/6x6TTT/playerClass.vb
@@ -21,40 +21,38 @@ Public Class playerClass
     'Returns:
     '   Display success
     Public Sub Register(ByVal username As String)
-        Dim usernameFile As New System.IO.StreamReader("nameofuser.txt")
-        Dim numberofUsers = 0, yesFlag, noFlag As Integer
-        yesFlag = 0
-        noFlag = 0
-        Dim StrdUsername As String = ""
-        Dim NewUsername As String = ""
-        'BEGINHERE:
-
-        '        If (NewUsername = "") Then
-        '            MsgBox("Username cannot be blank")
-        '            GoTo BEGINHERE
-        '        End If
-        NewUsername = username.ToUpper()
-        Do Until usernameFile.Peek = -1
-            StrdUsername = usernameFile.ReadLine()
-            StrdUsername = StrdUsername.ToUpper()
-            If StrdUsername = NewUsername Then
-                yesFlag += 1
+        Try
+            Dim usernameFile As New System.IO.StreamReader("nameofuser.txt")
+            Dim numberofUsers = 0, yesFlag, noFlag As Integer
+            yesFlag = 0
+            noFlag = 0
+            Dim StrdUsername As String = ""
+            Dim NewUsername As String = ""
+            NewUsername = username.ToUpper()
+            Do Until usernameFile.Peek = -1
+                StrdUsername = usernameFile.ReadLine()
+                StrdUsername = StrdUsername.ToUpper()
+                If StrdUsername = NewUsername Then
+                    yesFlag += 1
+                Else
+                    noFlag = 0
+                End If
+                numberofUsers += 1
+            Loop
+            usernameFile.Close()
+            If (yesFlag > 0) Then
+                MsgBox("Username exist. Please enter a different name")
             Else
-                noFlag = 0
+                Dim file As New System.IO.StreamWriter("nameofuser.txt", True)
+                file.WriteLine(NewUsername)
+                file.Close()
+                loginMenuForm.Close()
+                mainMenuForm.Show()
+                MsgBox(" REGISTRATION WAS SUCESSFUL!" & ControlChars.CrLf & " THANK YOU FOR REGISTERING." & ControlChars.CrLf & " ENJOY THE GAME!")
             End If
-            numberofUsers += 1
-        Loop
-        usernameFile.Close()
-        If (yesFlag > 0) Then
-            MsgBox("Username exist. Please enter a different name")
-        Else
-            Dim file As New System.IO.StreamWriter("nameofuser.txt", True)
-            file.WriteLine(NewUsername)
-            file.Close()
-            loginMenuForm.Close()
-            mainMenuForm.Show()
-            MsgBox(" REGISTRATION WAS SUCESSFUL!" & ControlChars.CrLf & " THANK YOU FOR REGISTERING." & ControlChars.CrLf & " ENJOY THE GAME!")
-        End If
+        Catch
+            loginMenuForm.errorLabel.Text = "File not found."
+        End Try
     End Sub
 
     'Log in
@@ -68,7 +66,6 @@ Public Class playerClass
     'Returns:
     '   True if username is found
     Public Function Login(ByVal username As String)
-
         Try
             Dim usernameFile As New System.IO.StreamReader("nameofuser.txt")
             Dim numberofUsers = 0, yesFlag = 0, noFlag As Integer = 0
@@ -95,37 +92,6 @@ Public Class playerClass
             loginMenuForm.errorLabel.Text = "File not found."
             Return False
         End Try
-
-        'Dim i = 0, yesFlag, noFlag As Integer
-        'yesFlag = 0
-        'noFlag = 0
-        'Dim StrdUsername As String = ""
-        'Dim NewUsername As String = ""
-        'Dim users(20) As String
-        'BEGINHERE:
-
-        '           GoTo BEGINHERE
-
-        'Do Until usernameFile.Peek = -1
-        '    StrdUsername = usernameFile.ReadLine()
-        '    StrdUsername = StrdUsername.ToUpper
-        '    If StrdUsername = NewUsername Then
-        '        yesFlag = yesFlag + 1
-        '    Else
-        '        noFlag = 0
-        '    End If
-        '    i = i + 1
-        'Loop
-        'usernameFile.Close()
-        'If (yesFlag > 0) Then
-        '    MsgBox("Please Enter another username different than before")
-        'Else
-        '    'usernameFile.Close()
-        '    'Dim file As New StreamWriter("nameofuser.txt", True)
-        '    'file.WriteLine(NewUsername)
-        '    'file.Close()
-        '    'MsgBox(" REGISTRATION WAS SUCESSFUL" & ControlChars.CrLf & " THANK YOU FOR THE REGIusernameFileATION WITH VADER TIC-TAC-TOE" & ControlChars.CrLf & " ENJOY THE GAME")
-        'End If
     End Function
 
     'SetVader
@@ -138,25 +104,5 @@ Public Class playerClass
         name = "Vader"
     End Sub
 
-    Private Sub searchFile()
-        '    Try
-        '        Dim usernameFile = IO.File.ReadAllText("username.txt")
-        '        'Dim lookfor As String = username
-        '        ' If usernameFile.Contains(lookfor) Then
-        '        loginMenuForm.errorLabel.Text = "Username found"
-        '        loginMenuForm.errorLabel.Visible = False
 
-        '        Else
-        '        loginMenuForm.errorLabel.Text = "Username not found"
-        '        loginMenuForm.errorLabel.Visible = True
-        '        End If
-        '        'Using usernameFile As New System.IO.StreamWriter("username.txt")
-        '        '    usernameFile.WriteLine(username)
-        '        'End Using
-        '    Catch
-        '        errorLabel.Text = "Error with file"
-
-        '    End Try
-
-    End Sub
 End Class
